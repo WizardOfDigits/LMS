286a5720aa53a540635d17e3595e7f778476997b
WizardOfDigits <saurav.stamang@gmail.com>
2025-02-17 00:26:18 +0545

    feat: Implement db

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { app } from "./app";
-require("dotenv").config();
+import connectDB from "./utils/db";
 
 // create server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
 });
