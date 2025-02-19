e3b4fbf51e76e2d40d57a141317260c279fb116e
WizardOfDigits <saurav.stamang@gmail.com>
2025-02-20 00:48:44 +0545

    feat: Add routes for /user

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 export const app = express();
 
@@ -12,14 +13,17 @@ app.use(express.json({ limit: "50mb" }));
 // cookie parser
 app.use(cookieParser());
 
-// cors(cross origin resource sharing)
+// cors (cross origin resource sharing)
 app.use(
   cors({
     origin: process.env.ORIGIN,
   }),
 );
 
-// testing api
+// routes
+app.use("/api/v1", userRouter);
+
+// testing API
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
     success: true,
