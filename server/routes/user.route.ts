fc9d9869537919459ca25b5fa9089ace7531df23
WizardOfDigits <saurav.stamang@gmail.com>
2025-02-22 23:17:23 +0545

    feat: Add routes for (/activate-user,/login,/logout) for user

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,7 +1,15 @@
 import express from "express";
-import { registrationUser } from "../controllers/user.controller";
+import {
+  activateUser,
+  registrationUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller";
 
 const userRouter = express.Router();
 userRouter.post("/register", registrationUser);
+userRouter.post("/activate-user", activateUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", logoutUser);
 
 export default userRouter;
