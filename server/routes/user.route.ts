71d63de52f772ea4be39bfa8227be19e39c3950f
WizardOfDigits <saurav.stamang@gmail.com>
2025-03-02 15:19:11 +0545

    feat: Add routes for socailAuth,logout,refresh token,userinfo and update-user-info

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -4,12 +4,21 @@ import {
   registrationUser,
   loginUser,
   logoutUser,
+  updateAccessToken,
+  getUserInfo,
+  socialAuth,
+  updateUserInfo,
 } from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const userRouter = express.Router();
 userRouter.post("/register", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/logout", logoutUser);
+userRouter.post("/socialAuth", socialAuth);
+userRouter.post("/logout", isAuthenticated, logoutUser);
+userRouter.get("/refresh", updateAccessToken);
+userRouter.get("/info", isAuthenticated, getUserInfo);
+userRouter.post("/update-user-info", isAuthenticated, updateUserInfo);
 
 export default userRouter;
