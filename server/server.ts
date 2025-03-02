0f6af9654737ec77f08ee24e4264e74c91d28b79
WizardOfDigits <saurav.stamang@gmail.com>
2025-03-02 16:35:30 +0545

    feat: Add cloudinary config for user avatar

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import { app } from "./app";
 import connectDB from "./utils/db";
+import { v2 as cloudinary } from "cloudinary";
 
 // create server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
   connectDB();
+});
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
