868e191b0e16d4565a2badb9d4929db3317f5b59
WizardOfDigits <saurav.stamang@gmail.com>
2025-03-02 15:15:40 +0545

    fix: Corrected spelling mistakes and added updateAccessToken , get user info, login using social auth and update user info features

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -4,11 +4,17 @@ import { Request, Response, NextFunction } from "express";
 import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
-import jwt, { Secret } from "jsonwebtoken";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
-import { sendToken } from "../utils/jwt";
+import {
+  accessTokenOptions,
+  refreshTokenOptions,
+  sendToken,
+} from "../utils/jwt";
+import { redis } from "../utils/redis";
+import { getUserById } from "../services/user.service";
 
 // Register user
 interface IRegistrationBody {
@@ -52,8 +58,7 @@ export const registrationUser = CatchAsyncError(
       });
       res.status(201).json({
         success: true,
-        message:
-          "Please check your email:${user.email} to activate your account",
+        message: `Please check your email:${user.email} to activate your account`,
         activationToken: activationToken.token,
       });
     } catch (error) {
@@ -74,6 +79,9 @@ interface IActivationToken {
 export const createActivationToken = (
   user: IRegistrationBody,
 ): IActivationToken => {
+  if (!process.env.ACTIVATION_SECRET) {
+    throw new Error("ACTIVATION_SECRET is not defined");
+  }
   const activationCode = Math.floor(1000 + Math.random() * 9000).toString(); // Generate activation code
   const token = jwt.sign(
     { user, activationCode }, // Include activationCode in the token payload
@@ -107,7 +115,7 @@ export const activateUser = CatchAsyncError(
       const { name, email, password } = newUser.user;
       const existUser = await userModel.findOne({ email });
       if (existUser) {
-        return next(new ErrorHandler("Email already existe", 400));
+        return next(new ErrorHandler("Email already exists", 400));
       }
       const user = await userModel.create({
         name,
@@ -121,7 +129,7 @@ export const activateUser = CatchAsyncError(
       if (error instanceof Error) {
         return next(new ErrorHandler(error.message, 400));
       }
-      return next(new ErrorHandler("An unknown error occurred", 400));
+      return next(new ErrorHandler("An unknown error occurred", 500));
     }
   },
 );
@@ -152,7 +160,7 @@ export const loginUser = CatchAsyncError(
       if (error instanceof Error) {
         return next(new ErrorHandler(error.message, 400));
       }
-      return next(new ErrorHandler("An unknown error occurred", 400));
+      return next(new ErrorHandler("An unknown error occurred", 500));
     }
   },
 );
@@ -162,7 +170,10 @@ export const logoutUser = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       res.cookie("access_token", "", { maxAge: 1 });
-      res.cookie("refres_token", "", { maxAge: 1 });
+      res.cookie("refresh_token", "", { maxAge: 1 });
+
+      const userId = req.user?._id as string;
+      redis.del(userId);
       res.status(200).json({
         success: true,
         message: "Logout successfully",
@@ -171,7 +182,143 @@ export const logoutUser = CatchAsyncError(
       if (error instanceof Error) {
         return next(new ErrorHandler(error.message, 400));
       }
-      return next(new ErrorHandler("An unknown error occurred", 400));
+      return next(new ErrorHandler("An unknown error occurred", 500));
+    }
+  },
+);
+
+// update access token
+export const updateAccessToken = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const refresh_token = req.cookies.refresh_token as string;
+
+      const decoded = jwt.verify(
+        refresh_token,
+        process.env.REFRESH_TOKEN as string,
+      ) as JwtPayload;
+
+      const message = "Couldn't update refresh token";
+
+      if (!decoded) {
+        return next(new ErrorHandler(message, 400));
+      }
+
+      const session = await redis.get(decoded.id as string);
+      if (!session) {
+        return next(new ErrorHandler(message, 400));
+      }
+      const user = JSON.parse(session);
+
+      const accessToken = jwt.sign(
+        { id: user._id },
+        process.env.ACCESS_TOKEN as string,
+        {
+          expiresIn: "30m",
+        },
+      );
+
+      const refreshToken = jwt.sign(
+        { id: user._id },
+        process.env.REFRESH_TOKEN as string,
+        { expiresIn: "7d" },
+      );
+
+      res.cookie("access_token", accessToken, accessTokenOptions);
+      res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+      res.status(200).json({
+        status: "success",
+        accessToken,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+      return next(new ErrorHandler("An unknown error occurred", 500));
+    }
+  },
+);
+
+// get user info
+export const getUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.user._id;
+      getUserById(userId, res);
+    } catch (error) {
+      if (error instanceof Error) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+      return next(new ErrorHandler("An unknown error occurred", 500));
+    }
+  },
+);
+
+interface ISocalAuthBody {
+  email: string;
+  name: string;
+  avatar: string;
+}
+
+// socail auth
+export const socialAuth = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, name, avatar } = req.body as ISocalAuthBody;
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        const newUser = await userModel.create({
+          email,
+          name,
+          avatar,
+        });
+        sendToken(newUser, 200, res);
+      } else {
+        sendToken(user, 200, res);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+      return next(new ErrorHandler("An unknown error occurred", 500));
+    }
+  },
+);
+
+// update user info
+interface IUpdateUserInfo {
+  name?: string;
+  email?: string;
+}
+export const updateUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email } = req.body as IUpdateUserInfo;
+      const userId = req.user?._id;
+      const user = await userModel.findById(userId);
+
+      if (email && user) {
+        const isEmailExist = await userModel.findOne({ email });
+        if (isEmailExist) {
+          return next(new ErrorHandler("Email already exists", 400));
+        }
+        user.email = email;
+      }
+      if (name && user) {
+        user.name = name;
+      }
+      await user?.save();
+      await redis.set(userId, JSON.stringify(user));
+      res.status(201).json({
+        success: true,
+        user,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+      return next(new ErrorHandler("An unknown error occurred", 500));
     }
   },
 );
