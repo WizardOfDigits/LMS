f509411de7c2038093bf14f23538703cebca5aae
WizardOfDigits <saurav.stamang@gmail.com>
2025-03-17 23:38:06 +0545

    feat: Integrate course routes into application

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "./middleware/error";
 import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
 
 export const app = express();
 
@@ -22,6 +23,7 @@ app.use(
 
 // routes
 app.use("/api/v1", userRouter);
+app.use("/api/v1", courseRouter);
 
 // testing API
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
