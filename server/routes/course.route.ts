ace02780845b8e2cacc0fc8442b6bcbde1015587
WizardOfDigits <saurav.stamang@gmail.com>
2025-03-21 00:00:43 +0545

    feat: Add routes for course content, Q&A, reviews, and replies

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -4,6 +4,11 @@ import {
   editCourse,
   getSingleCourse,
   getAllCourses,
+  getCourseByUser,
+  addQuestion,
+  addAnswer,
+  addReview,
+  addReplyToReview,
 } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
@@ -30,5 +35,25 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 
 // get all coursess
 courseRouter.get("/get-courses/", getAllCourses);
+
+// get course content
+courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
+
+// add question in course
+courseRouter.put("/add-question", isAuthenticated, addQuestion);
+
+// send answer replay through mail
+courseRouter.put("/add-answer", isAuthenticated, addAnswer);
+
+// add Review
+courseRouter.put("/add-review/:id", isAuthenticated, addReview);
+
+// add replay to Review
+courseRouter.put(
+  "/add-replay-review",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  addReplyToReview,
+);
 
 export default courseRouter;
