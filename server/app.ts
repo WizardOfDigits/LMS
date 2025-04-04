61d13ce430d7d4be31f7c3d56b38f97d0c9d1416
WizardOfDigits <saurav.stamang@gmail.com>
2025-04-04 06:48:19 +0545

    feat: Add notification router

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,7 @@ import errorMiddleware from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
+import notficationRouter from "./routes/notification.route";
 
 export const app = express();
 
@@ -23,7 +24,7 @@ app.use(
 );
 
 // routes
-app.use("/api/v1", userRouter, courseRouter, orderRouter);
+app.use("/api/v1", userRouter, courseRouter, orderRouter, notficationRouter);
 
 // testing API
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
